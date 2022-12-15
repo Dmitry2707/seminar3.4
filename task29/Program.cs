9286a9838ff52a6cb67d9cbec0a920fe2181ac7a
Dmitry2707 <kemper1495@gmail.com>
2022-12-15 23:49:51 +0300

    task29

---
+++ task29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число C");
+int c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("[" + a + "," + b + "," + c + "]");
